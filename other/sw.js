cbabc2fdf3325e176ce5c999056c9bc1a40e800e
Julian Chennales <jchennales@geturgently.com>
2025-10-14 09:55:38 -0300

    Allow adding score in tally by clicking anywhere in the squares.

---
+++ other/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'scorekeeper-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
